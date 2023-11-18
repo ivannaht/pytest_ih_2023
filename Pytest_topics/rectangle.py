4b9669c27444cd3abb78c7fd0fcea76f21545772
ihont <ihont@softserveinc.com>
2023-11-18 22:22:08 +0200

    Added tests with exceptions

--- Pytest_topics/rectangle.py
+++ Pytest_topics/rectangle.py
@@ -1,3 +1,4 @@
+import pytest
 class Rectangle:
 
     def __init__(self, length, width):
@@ -5,7 +6,19 @@ class Rectangle:
         self.width = width
 
     def calculate_rectangle_area(self):
+        if not isinstance(self.length, (int, float)):
+            raise TypeError("Length must be a number")
+        if not isinstance(self.width, (int, float)):
+            raise TypeError("Width must be a number")
+        if self.length <= 0 or self.width <= 0:
+            raise ValueError("Length and width must be positive")
         return self.length * self.width
 
     def calculate_rectangle_perimeter(self):
+        if not isinstance(self.length, (int, float)):
+            raise TypeError("Length must be a number")
+        if not isinstance(self.width, (int, float)):
+            raise TypeError("Width must be a number")
+        if self.length <= 0 or self.width <= 0:
+            raise ValueError("Length and width must be positive")
         return 2 * (self.length + self.width)
