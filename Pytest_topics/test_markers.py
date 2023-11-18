06d9be3ad9d5adbbb637f502f14b7983b7892908
ihont <ihont@softserveinc.com>
2023-11-18 21:41:10 +0200

    Added markers

--- Pytest_topics/test_markers.py
+++ Pytest_topics/test_markers.py
@@ -1,45 +1,64 @@
-import math
-
-from Pytest_topics.rectangle import Rectangle
+import pytest
 from Pytest_topics.circle import Circle
+from Pytest_topics.rectangle import Rectangle
+pytestmark = [pytest.mark.rectangle, pytest.mark.circle]
 
 
+# verify rectangle
+@pytest.mark.smoke
+@pytest.mark.rectangle
 def test_case01():
-    rectangle1 = Rectangle(4, 10)
-    assert rectangle1.calculate_rectangle_area() == 40, 'verify correct area - passed'
+    rect1 = Rectangle(4, 10)
+    assert rect1.calculate_rectangle_area() == 40, \
+        'verify correct area - passed'
 
 
+@pytest.mark.rectangle
 def test_case02():
-    rectangle2 = Rectangle(4, 5)
-    assert rectangle2.calculate_rectangle_area() == 15, 'verify incorrect area - failed'
+    rect2 = Rectangle(4, 5)
+    assert rect2.calculate_rectangle_area() == 15, \
+        'verify incorrect area - failed'
 
 
+@pytest.mark.rectangle
 def test_case03():
-    rectangle3 = Rectangle(5, 10)
-    assert rectangle3.calculate_rectangle_perimeter() == 30, 'verify correct perimeter - passed'
+    rect3 = Rectangle(5, 10)
+    assert rect3.calculate_rectangle_perimeter() == 30, \
+        'verify correct perimeter - passed'
 
 
+@pytest.mark.rectangle
 def test_case04():
-    rectangle4 = Rectangle(2, 5)
-    assert rectangle4.calculate_rectangle_perimeter() == 10, 'verify incorrect perimeter - failed'
+    rect4 = Rectangle(2, 5)
+    assert rect4.calculate_rectangle_perimeter() == 10, \
+        'verify incorrect perimeter - failed'
 
 
 # verify circle
+@pytest.mark.smoke
+@pytest.mark.circle
 def test_case05():
-    circle1 = Circle(4)
-    assert round(circle1.calculate_circle_area(), 2) == 50.27, 'verify correct area - passed'
+    cir1 = Circle(4)
+    assert round(cir1.calculate_circle_area(), 2) == 50.27, \
+        'verify correct area - passed'
 
 
+@pytest.mark.circle
 def test_case06():
-    circle2 = Circle(5)
-    assert circle2.calculate_circle_area() == 15, 'verify incorrect area - failed'
+    cir2 = Circle(5)
+    assert cir2.calculate_circle_area() == 15, \
+        'verify incorrect area - failed'
 
 
+@pytest.mark.circle
 def test_case07():
-    circle3 = Circle(10)
-    assert round(circle3.calculate_circle_perimeter(), 2) == 62.83, 'verify correct perimeter - passed'
+    cir3 = Circle(10)
+    assert round(cir3.calculate_circle_perimeter(), 2) == 62.83, \
+        'verify correct perimeter - passed'
 
 
+@pytest.mark.circle
 def test_case08():
-    circle4 = Circle(11)
-    assert circle4.calculate_circle_perimeter() == 70, 'verify incorrect perimeter - failed'
+    cir4 = Circle(11)
+    assert cir4.calculate_circle_perimeter() == 70, \
+        'verify incorrect perimeter - failed'
