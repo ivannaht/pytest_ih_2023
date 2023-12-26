0db53f2fddf77013299780c50f8461c6e69700e8
ihont <ihont@softserveinc.com>
2023-12-26 15:49:58 +0200

    [main] Added logging with BaseClass

---
+++ Pytest_topics/base.py
@@ -0,0 +1,16 @@
+import inspect
+import logging
+
+
+class BaseClass:
+
+    def get_logger(self):
+        logger_name = inspect.stack()[1][3]
+        logger = logging.getLogger(logger_name)
+        file_handler = logging.FileHandler("logfile.log")
+        formatter = logging.Formatter("%(asctime)s: %(levelname)s: %(name)s: %(message)s")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        logger.setLevel(logging.DEBUG)
+
+        return logger
--- Pytest_topics/test_fixtures_with_data.py
+++ Pytest_topics/test_fixtures_with_data.py
@@ -1,13 +1,17 @@
 import pytest
 
+from Pytest_topics.base import BaseClass
+
 
 @pytest.mark.usefixtures("user_data")
-class TestFixtureWithData:
+class TestFixtureWithData(BaseClass):
 
     def test_case01(self, user_data):
-        print(user_data)
+        log = self.get_logger()
+        log.info(user_data)
         assert 2 + 2 == 4
 
     def test_case02(self, user_data):
-        print(user_data)
+        log = self.get_logger()
+        log.info(user_data)
         assert 2 - 2 == 0
