45c02b40691fb33814cfeb9af9f2113206382c57
ihont <ihont@softserveinc.com>
2023-12-29 17:26:12 +0200

    [main] Updated Excel file processing

--- Pytest_topics/users_excel.py
+++ Pytest_topics/users_excel.py
@@ -10,24 +10,28 @@ book = openpyxl.load_workbook(data_file)
 sheet = book.active
 
 
-def get_usernames():
+def get_users():
     username_list = []
     for i in range(2, sheet.max_row + 1):
         username = sheet.cell(row=i, column=1).value
-        username_list.append(username)
+        first_name = sheet.cell(row=i, column=2).value
+        last_name = sheet.cell(row=i, column=3).value
+        username_list.append({'username': username, 'first_name': first_name, 'last_name': last_name})
     return username_list
 
 
 def parse_excel_to_dict(username):
-    Dict1 = {}
+    user_info = {}
     for i in range(2, sheet.max_row + 1):
         if sheet.cell(row=i, column=1).value == username:
             for j in range(1, sheet.max_column + 1):
-                Dict1[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
-    return Dict1
+                user_info[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
+    return user_info
 
 
-selected_username = sheet['A6'].value;
+selected_username = sheet['A6'].value
+users = get_users()
 
-print(get_usernames())
+print(users)
 print(parse_excel_to_dict(selected_username))
+print(parse_excel_to_dict(users[4]['username']))
